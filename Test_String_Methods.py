692f350c2d670394049c8dbe32bb0096de2e5472
antoni <antoni@example.com>
2019-12-14 14:11:21 +0100

    Git modified two failing methods and added new test for Startswith()

--- Test_String_Methods.py
+++ Test_String_Methods.py
@@ -12,16 +12,21 @@ class Test_String(unittest.TestCase):
         self.assertEqual(view, view.casefold())
 
     def test_scen(self):
-        view = misiek.scen('wyskok', 2)
-        self.assertEqual(view, view.center())
+        view = misiek.scen('wyskok', 20)
+        self.assertNotEqual(view, view.center(2))
 
     def test_scou(self):
         view = misiek.scou('Ala ma kota a kot ma ala', 'ma')
-        self.assertEqual(view, view.count())
+        self.assertEqual(view, view.count('ma'))
 
     def test_senc(self):
         view = misiek.senc('kanalizacja')
         self.assertEqual(view, view.encode())
+
+    def test_ssta(self):
+        view = misiek.ssta('Wellcom in Brave New World!', 'Wellcom')
+        self.assertTrue(view)
+
 
 if __name__ == '__main__':
     unittest.main()
