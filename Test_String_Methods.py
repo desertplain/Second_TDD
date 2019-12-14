d41586e0507718cbec865d28dfb9844e08370bed
antoni <antoni@example.com>
2019-12-14 13:44:32 +0100

    Small changes to Unittest Repo

--- Test_String_Methods.py
+++ Test_String_Methods.py
@@ -1,11 +1,27 @@
 import unittest
-import string
+import misiek
 
 class Test_String(unittest.TestCase):
 
     def test_scap(self):
-        view = string.scap()
-        self.assertEqual(view, capitalize(view))
+        view = misiek.scap('rzeczy')
+        self.assertEqual(view, view.capitalize())
+
+    def test_scas(self):
+        view = misiek.scas('strachy na lachy')
+        self.assertEqual(view, view.casefold())
+
+    def test_scen(self):
+        view = misiek.scen('wyskok')
+        self.assertEqual(view, view.center())
+
+    def test_scou(self):
+        view = misiek.scou('napoje')
+        self.assertEqual(view, view.count())
+
+    def test_senc(self):
+        view = misiek.senc('kanalizacja')
+        self.assertEqual(view, view.encode())
 
 if __name__ == '__main__':
     unittest.main()
